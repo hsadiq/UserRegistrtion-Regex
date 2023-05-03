27c9db527620cfa851f6711d0a2c4e0140a08885
hsadiq <ksadik320@gmail.com>
2023-05-03 20:10:18 +0530

    Add Valid First Name code by using Regex

--- src/com/bl/UserRegistration.java
+++ src/com/bl/UserRegistration.java
@@ -6,7 +6,7 @@ public class UserRegistration {
 
         ValidateUserInfo user = new ValidateUserInfo();
 
-
+        user.checkFirstName();
 
     }
 }
--- src/com/bl/ValidateUserInfo.java
+++ src/com/bl/ValidateUserInfo.java
@@ -6,5 +6,16 @@ import java.util.regex.*;
 public class ValidateUserInfo {
     Scanner sc = new Scanner(System.in);
 
+    public void checkFirstName(){
+        System.out.println("Enter The First Name: ");
+        String firstName = sc.nextLine();
 
+
+
+        if(Pattern.matches("^[A-Z] {1} [a-z] {3,}", firstName)){
+            System.out.println("First Name is Valid");
+        }else {
+            System.out.println("Invalid Input");
+        }
+    }
 }
