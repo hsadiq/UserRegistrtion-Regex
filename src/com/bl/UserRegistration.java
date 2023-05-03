d1681bbf848efbfca7049b17bf73169ab6787735
hsadiq <ksadik320@gmail.com>
2023-05-03 20:06:11 +0530

    Added UserRegistration Code

---
+++ src/com/bl/UserRegistration.java
@@ -0,0 +1,12 @@
+package com.bl;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome To User Registration Programme");
+
+        ValidateUserInfo user = new ValidateUserInfo();
+
+
+
+    }
+}
---
+++ src/com/bl/ValidateUserInfo.java
@@ -0,0 +1,10 @@
+package com.bl;
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class ValidateUserInfo {
+    Scanner sc = new Scanner(System.in);
+
+
+}
